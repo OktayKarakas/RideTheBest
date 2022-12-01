3c2df78e0d0aad60c006ba1af34ddec15b9dfb83
catresium <oktaykarakas71@gmail.com>
2022-12-01 23:26:49 +0300

    create TravellMe && create header

---
+++ src/components/Modal.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <div className="modal__body">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam,
+        maiores molestiae. Repellat deserunt aut pariatur velit qui a? Labore,
+        ab.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam,
+        maiores molestiae.
+      </div>
+    </div>
+  )
+}
+
+export default Modal
---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Header from '../components/Header'
+import Modal from '../components/Modal'
+
+const Home = () => {
+  const [state] = useState({
+    heading: 'We are travel friends',
+    paragraph:
+      'Come and join us we travel the most famous and beautiful places  in the world',
+  })
+  return (
+    <>
+      <Header heading={state.heading} paragraph={state.paragraph}>
+        <button className="btn-default">Get Started</button>
+      </Header>
+      <Modal />
+    </>
+  )
+}
+
+export default Home
